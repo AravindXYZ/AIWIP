b383e682305f326908af7bb654c01d56e284b079
AravindXYZ <aravind.g.xyz@gmail.com>
2025-03-19 19:43:00 +0530

    Added some first tests.

---
+++ MLTests/DecisionTree/DecisionTree.py
@@ -0,0 +1,47 @@
+import pandas as pd
+dataset = pd.read_csv("Datasets/insurance_pre.csv")
+#print(dataset)
+dummies = pd.get_dummies(dataset,drop_first=True)
+dummies = dummies.astype(int)
+#print(dummies)
+independent=dummies[['age', 'bmi', 'children','sex_male', 'smoker_yes']]
+dependent=dataset['charges']
+#print(type(dummies))
+print(dependent)
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(independent,dependent,test_size=0.30,random_state=0)
+
+from sklearn.tree import DecisionTreeRegressor
+regressor=DecisionTreeRegressor()#criterion='friedman_mse', splitter='random'
+regressor.fit(X_train,y_train)
+
+import matplotlib.pyplot as plt
+from sklearn import tree
+tree.plot_tree(regressor)
+plt.show()
+
+y_pred=regressor.predict(X_test)
+#print('y_pred',y_pred)
+
+from sklearn.metrics import r2_score
+r_score=r2_score(y_test,y_pred)
+
+print('r_score',r_score)
+
+import pickle
+filePath="Deployed/DT.sav"
+pickle.dump(regressor,open(filePath,'wb'))
+
+loaded_model=pickle.load(open(filePath,'rb'))
+data = [[30, 25, 2, 1, 0],[45, 30, 0, 0, 1]]#
+df = pd.DataFrame(data,columns=['age', 'bmi', 'children', 'sex_male', 'smoker_yes'])
+result=loaded_model.predict(df)#or passin #X_test
+
+for d in range(len(data)):
+    print("-------------------")
+    for i in range(df.columns.size):
+        print(df.columns[i]+" = ",data[d][i])
+    print("cost = ",result[d])
+print("-------------------")
+print('r_score2',r2_score([4500,29000],result))
